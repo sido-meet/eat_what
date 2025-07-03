256b44714e449c70792d1d451191f215c1f89b9e
sido <sido.meet@gmail.com>
2025-07-03 11:40:38 +0800

    fix: Listen on all network interfaces (0.0.0.0)

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,6 @@ app.use(express.json()); // For parsing application/json
 app.use('/api', foodRoutes);
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
